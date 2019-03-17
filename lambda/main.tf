1c5b443ba246c9c756f9bf0b1e8cf95ea9f7fe55
James Collerton <jamescollerton@live.co.uk>
2019-03-17 09:21:47 +0000

    Added variables back in, it was the terraform in the lambda that was causing problems

--- lambda/main.tf
+++ lambda/main.tf
@@ -18,26 +18,11 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  filename         = "lambda_function_payload.zip"
-  function_name    = "lambda_function_name"
+resource "aws_lambda_function" "aws_lambda_function" {
+  filename         = "${var.lambda_function_filename}"
+  function_name    = "${var.lambda_function_name}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "exports.test"
+  handler          = "${var.lambda_function_handler}"
   source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  runtime          = "nodejs8.10"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
+  runtime          = "java8"
 }
-
-#resource "aws_lambda_function" "aws_lambda_function" {
-#  filename         = "${var.lambda_function_filename}"
-#  function_name    = "${var.lambda_function_name}"
-#  role             = "${aws_iam_role.iam_for_lambda.arn}"
-#  handler          = "${var.lambda_function_handler}"
-#  source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-#  runtime          = "java8"
-#}
