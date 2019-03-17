3a767b2c328decbb8f2e3f97659ae75d953f725e
James Collerton <jamescollerton@live.co.uk>
2019-03-17 09:49:42 +0000

    Added back in filename

--- lambda/main.tf
+++ lambda/main.tf
@@ -19,10 +19,10 @@ EOF
 }
 
 resource "aws_lambda_function" "aws_lambda_function" {
-#  filename         = "${var.lambda_function_filename}"
+  filename         = "${var.lambda_function_filename}"
   function_name    = "${var.lambda_function_name}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "${var.lambda_function_handler}"
-#  source_code_hash = "${base64sha256(file(var.lambda_function_filename))}"
+  source_code_hash = "${base64sha256(file(var.lambda_function_filename))}"
   runtime          = "java8"
 }
