220bb626df26cfcab5faaee38d11590ce4c9d342
James Collerton <jamescollerton@live.co.uk>
2019-03-17 09:03:52 +0000

    Updated lambda to not have nested evaluation

--- lambda/main.tf
+++ lambda/main.tf
@@ -23,6 +23,6 @@ resource "aws_lambda_function" "${var.lambda_function_name}" {
   function_name    = "${var.lambda_function_name}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "${var.lambda_function_handler}"
-  source_code_hash = "${base64sha256(file(${var.lambda_function_filename})}"
+  source_code_hash = "${base64sha256(file(var.lambda_function_filename)}"
   runtime          = "java8"
 }
