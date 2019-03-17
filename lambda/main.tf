a4a5bdd69c49ac2c6bad57a33607a99c5552a3e7
James Collerton <jamescollerton@live.co.uk>
2019-03-17 09:18:43 +0000

    Removed all variables

--- lambda/main.tf
+++ lambda/main.tf
@@ -18,11 +18,26 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "aws_lambda_function" {
-  filename         = "${var.lambda_function_filename}"
-  function_name    = "${var.lambda_function_name}"
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "lambda_function_name"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "${var.lambda_function_handler}"
+  handler          = "exports.test"
   source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  runtime          = "java8"
+  runtime          = "nodejs8.10"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
 }
+
+#resource "aws_lambda_function" "aws_lambda_function" {
+#  filename         = "${var.lambda_function_filename}"
+#  function_name    = "${var.lambda_function_name}"
+#  role             = "${aws_iam_role.iam_for_lambda.arn}"
+#  handler          = "${var.lambda_function_handler}"
+#  source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+#  runtime          = "java8"
+#}
