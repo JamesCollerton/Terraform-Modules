1f4a0fe365caa0009bd3f6ab9014316dfd83189e
James Collerton <jamescollerton@live.co.uk>
2019-03-19 07:34:27 +0000

    Added API Gateway module

---
+++ apigateway/variables.tf
@@ -0,0 +1,10 @@
+variable "api_gateway_name" {
+  description 	= "The name you would like to assign to your API gateway"
+  type 		= "string" 
+}
+
+description = "api_gateway_description" {
+  description 	= "The description of your API gateway"
+  type 		= "string" 
+}
+
