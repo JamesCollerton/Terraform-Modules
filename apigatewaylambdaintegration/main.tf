01e12f9ce7f6f0629d658bf66dc1e5d608036f6c
James Collerton <jamescollerton@live.co.uk>
2019-03-21 07:28:26 +0000

    Halfway through writing the lambda integration

---
+++ apigatewaylambdaintegration/main.tf
@@ -0,0 +1,22 @@
+resource "aws_api_gateway_resource" "aws_api_gateway_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+  parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "aws_api_gateway_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_aws_api_gateway_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.example.invoke_arn}"
+}
