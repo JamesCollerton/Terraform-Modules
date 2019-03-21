cb822a0af2b518e1d2cfd3ca3a9d5f12b43e277f
James Collerton <jamescollerton@live.co.uk>
2019-03-21 07:09:45 +0000

    Also added root resource Id as an output

--- apigateway/output.tf
+++ apigateway/output.tf
@@ -1,3 +1,8 @@
 output "id" {
   value = "${aws_api_gateway_rest_api.aws_api_gateway_rest_api.id}"
 }
+
+output "root_resource_id" {
+  value = "${aws_api_gateway_rest_api.aws_api_gateway_rest_api.root_resource_id}"
+}
+
