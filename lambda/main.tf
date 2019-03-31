95164870e08d907a6c2e3a0c99800ef28034e3d7
James Collerton <jamescollerton@live.co.uk>
2019-03-31 15:44:46 +0100

    removed source code hash

--- lambda/main.tf
+++ lambda/main.tf
@@ -5,6 +5,6 @@ resource "aws_lambda_function" "aws_lambda_function" {
   function_name    = "${var.lambda_function_name}"
   role             = "${var.iam_for_lambda_arn}"
   handler          = "${var.lambda_function_handler}"
-  source_code_hash = "${base64sha256(file(var.lambda_function_filename))}"
+#  source_code_hash = "${base64sha256(file(var.lambda_function_filename))}"
   runtime          = "java8"
 }
