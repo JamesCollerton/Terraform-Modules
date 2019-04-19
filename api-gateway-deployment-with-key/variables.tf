c6a59e865cb26577f96e27801f7e02a7802c566c
James Collerton <jamescollerton@live.co.uk>
2019-04-19 08:12:52 +0100

    Fixed variable names

--- api-gateway-deployment-with-key/variables.tf
+++ api-gateway-deployment-with-key/variables.tf
@@ -1,9 +1,9 @@
-variable "${var.api_gateway_id}" {
+variable "api_gateway_id" {
   type 		= "string"
   description 	= "The Id of the api gateway we want to create the deployment for"
 }
 
-variable "${var.stage_name}" {
+variable "stage_name" {
   type 		= "string"
   description 	= "The name of the stage to create"
 }
