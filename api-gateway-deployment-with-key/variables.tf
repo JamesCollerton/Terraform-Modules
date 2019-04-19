4b5ef7a02b470350af50465e2e82d910c544e33b
James Collerton <jamescollerton@live.co.uk>
2019-04-19 08:10:07 +0100

    Don't really need the dependencies

--- api-gateway-deployment-with-key/variables.tf
+++ api-gateway-deployment-with-key/variables.tf
@@ -1,8 +1,3 @@
-variable "${var.api_gateway_dependencies}" {
-  type 		= "list(string)"
-  description 	= "A list of dependencies to finish before the api gateway starts"
-}
-
 variable "${var.api_gateway_id}" {
   type 		= "string"
   description 	= "The Id of the api gateway we want to create the deployment for"
